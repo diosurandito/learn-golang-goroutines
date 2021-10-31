c292eec705177981308007a3217294654b26aaa2
diosurandito <diosurandito@gmail.com>
2021-10-31 16:17:13 +0700

    10.Channel in dan out

--- channel_test.go
+++ channel_test.go
@@ -36,3 +36,22 @@ func TestChannelAsParameter(t *testing.T) {
 	data := <-channel
 	fmt.Println(data)
 }
+
+func OnlyIn(channel chan<- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Dio Surandito"
+}
+
+func OnlyOut(channel <-chan string) {
+	data := <-channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+	time.Sleep(3 * time.Second)
+}
