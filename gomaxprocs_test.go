a21c754558f20c397d69c2ad7f5cebb5da94d10f
diosurandito <diosurandito@gmail.com>
2021-11-24 11:40:47 +0700

    27.GOMAXPROCS

---
+++ gomaxprocs_test.go
@@ -0,0 +1,54 @@
+package golang_goroutines
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestGetGomaxprocs(t *testing.T) {
+	group := sync.WaitGroup{}
+	for i := 0; i < 100; i++ {
+		group.Add(1)
+		go func() {
+			time.Sleep(3 * time.Second)
+			group.Done()
+		}()
+	}
+
+	totalCpu := runtime.NumCPU()
+	fmt.Println("CPU", totalCpu)
+
+	totalThread := runtime.GOMAXPROCS(-1)
+	fmt.Println("Thread", totalThread)
+
+	totalGoroutine := runtime.NumGoroutine()
+	fmt.Println("Goroutine", totalGoroutine)
+
+	group.Wait()
+}
+
+func TestChangeThreadNumber(t *testing.T) {
+	group := sync.WaitGroup{}
+	for i := 0; i < 100; i++ {
+		group.Add(1)
+		go func() {
+			time.Sleep(3 * time.Second)
+			group.Done()
+		}()
+	}
+
+	totalCpu := runtime.NumCPU()
+	fmt.Println("CPU", totalCpu)
+
+	runtime.GOMAXPROCS(20)
+	totalThread := runtime.GOMAXPROCS(-1)
+	fmt.Println("Thread", totalThread)
+
+	totalGoroutine := runtime.NumGoroutine()
+	fmt.Println("Goroutine", totalGoroutine)
+
+	group.Wait()
+}
